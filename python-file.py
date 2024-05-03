fc63690dc73810519e03f0da2b94252060f218c0
BaurRitto <baurzhan87@gmail.com>
2024-05-03 19:27:54 +0500

    Update python-file.py

--- python-file.py
+++ python-file.py
@@ -1,8 +1,10 @@
 
 import datetime
+import os
 
 def main():
     print(f"Current date and time: {datetime.datetime.now()}")
 
 if __name__ == "__main__":
     main()
+    print(os.getenv('PG_USER'))
